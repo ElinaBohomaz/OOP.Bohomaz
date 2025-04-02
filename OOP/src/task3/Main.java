fc6c5366d7d0e749c9b4e037b0f72d4305ef8f29
ElinaBohomaz <159908931+ElinaBohomaz@users.noreply.github.com>
2025-04-02 19:17:39 +0200

    Update files

--- OOP/src/task3/Main.java
+++ OOP/src/task3/Main.java
@@ -2,19 +2,36 @@ package task3;
 
 import java.io.*;
 import java.util.*;
-
+/**
+ * Інтерфейс для відображення результатів обчислень
+ */
 interface ResultDisplay {
     void displayBasic(Data data);
 }
 
+/**
+ * Інтерфейс фабрики для створення об'єктів відображення
+ */
 interface DisplayFactory {
     ResultDisplay createDisplay();
 }
 
+/**
+ * Абстрактний клас фабрики для створення об'єктів відображення
+ * Реалізує інтерфейс DisplayFactory
+ */
 abstract class ResultDisplayFactory implements DisplayFactory {
+    /**
+     * Абстрактний метод для створення об'єкта відображення
+     * @return об'єкт, що реалізує інтерфейс ResultDisplay
+     */
     public abstract ResultDisplay createDisplay();
 }
 
+/**
+ * Клас для відображення результатів у консоль
+ * Реалізує інтерфейс ResultDisplay
+ */
 class ConsoleResultDisplay implements ResultDisplay {
     @Override
     public void displayBasic(Data data) {
@@ -23,18 +40,33 @@ class ConsoleResultDisplay implements ResultDisplay {
         data.printTable();
         System.out.println("Сума: " + data.getResult());
         System.out.println("Кількість чисел: " + data.getNumbers().size());
-
     }
 }
 
+/**
+ * Фабрика для створення об'єктів відображення у консоль
+ * Розширює абстрактний клас ResultDisplayFactory
+ */
 class ConsoleResultDisplayFactory extends ResultDisplayFactory {
+    /**
+     * Створює об'єкт для відображення результатів у консоль
+     * @return об'єкт ConsoleResultDisplay
+     */
     @Override
     public ResultDisplay createDisplay() {
         return new ConsoleResultDisplay();
     }
 }
 
+/**
+ * Клас для відображення результатів у файл
+ * Реалізує інтерфейс ResultDisplay
+ */
 class FileResultDisplay implements ResultDisplay {
+    /**
+     * Відображає базову інформацію про дані у файл results.txt
+     * @param data дані для запису у файл
+     */
     @Override
     public void displayBasic(Data data) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("results.txt"))) {
@@ -50,6 +82,10 @@ class FileResultDisplay implements ResultDisplay {
     }
 }
 
+/**
+ * Фабрика для створення об'єктів відображення у файл
+ * Розширює абстрактний клас ResultDisplayFactory
+ */
 class FileResultDisplayFactory extends ResultDisplayFactory {
     @Override
     public ResultDisplay createDisplay() {
@@ -57,19 +93,36 @@ class FileResultDisplayFactory extends ResultDisplayFactory {
     }
 }
 
+/**
+ * Клас для зберігання та обробки даних
+ * Реалізує інтерфейс Serializable для можливості серіалізації
+ */
 class Data implements Serializable {
     private static final long serialVersionUID = 1L;
-    private List<Integer> numbers;
-    private int result;
-
+    private List<Integer> numbers; // Список чисел
+    private int result; // Результат обчислень (сума)
     public Data(List<Integer> numbers) {
         this.numbers = new ArrayList<>(numbers);
+        // Обчислення суми за допомогою Stream API
         this.result = numbers.stream().mapToInt(Integer::intValue).sum();
     }
 
+    /**
+     * Повертає незмінний список чисел
+     * @return незмінний список чисел
+     */
     public List<Integer> getNumbers() { return Collections.unmodifiableList(numbers); }
+
+    /**
+     * Повертає результат обчислень (суму)
+     * @return сума чисел
+     */
     public int getResult() { return result; }
 
+    /**
+     * Виводить таблицю чисел у консоль
+     * Розбиває числа на рядки по 3 числа в кожному
+     */
     public void printTable() {
         for (int i = 0; i < numbers.size(); i++) {
             System.out.printf("%4d ", numbers.get(i));
@@ -77,6 +130,12 @@ class Data implements Serializable {
         }
     }
 
+    /**
+     * Записує таблицю чисел у файл
+     * Розбиває числа на рядки по 3 числа в кожному
+     * @param writer об'єкт BufferedWriter для запису у файл
+     * @throws IOException якщо виникає помилка вводу/виводу
+     */
     public void printTable(BufferedWriter writer) throws IOException {
         for (int i = 0; i < numbers.size(); i++) {
             writer.write(String.format("%4d ", numbers.get(i)));
@@ -84,6 +143,11 @@ class Data implements Serializable {
         }
     }
 
+    /**
+     * Статичний метод для завантаження даних з файлу
+     * @param filename ім'я файлу для завантаження
+     * @return новий об'єкт Data з завантаженими даними
+     */
     public static Data loadDataFromFile(String filename) {
         List<Integer> numbers = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
@@ -105,6 +169,10 @@ class Data implements Serializable {
         return new Data(numbers);
     }
 
+    /**
+     * Оновлює дані з файлу, замінюючи поточні дані
+     * @param filename ім'я файлу для завантаження
+     */
     public void updateDataFromFile(String filename) {
         List<Integer> newNumbers = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
@@ -121,7 +189,6 @@ class Data implements Serializable {
                 }
             }
 
-            // Замість додавання, повністю замінюємо список
             this.numbers = newNumbers;
 
             // Перераховуємо нову суму після оновлення
@@ -132,20 +199,35 @@ class Data implements Serializable {
     }
 }
 
+/**
+ * Клас для обробки даних і відображення результатів
+ */
 class Calculator {
-    private Data data;
-    private ResultDisplayFactory displayFactory;
+    private Data data; // Дані для обробки
+    private ResultDisplayFactory displayFactory; // Фабрика для створення об'єктів відображення
 
+    /**
+     * Конструктор для створення калькулятора
+     * @param numbers список чисел для обробки
+     * @param displayFactory фабрика для створення об'єктів відображення
+     */
     public Calculator(List<Integer> numbers, ResultDisplayFactory displayFactory) {
         this.data = new Data(numbers);
         this.displayFactory = displayFactory;
     }
 
+    /**
+     * Відображає результати обчислень
+     */
     public void displayResults() {
         ResultDisplay display = displayFactory.createDisplay();
         display.displayBasic(data);
     }
 
+    /**
+     * Зберігає дані у файл
+     * @param filename ім'я файлу для збереження
+     */
     public void saveData(String filename) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
             for (Integer num : data.getNumbers()) {
@@ -160,27 +242,44 @@ class Calculator {
         }
     }
 
+    /**
+     * Завантажує дані з файлу
+     * @param filename ім'я файлу для завантаження
+     */
     public void loadData(String filename) {
         this.data = Data.loadDataFromFile(filename);
     }
 
+    /**
+     * Оновлює дані з файлу
+     * @param filename ім'я файлу для оновлення
+     */
     public void updateData(String filename) {
         this.data.updateDataFromFile(filename);
     }
 }
 
+/**
+ * Головний клас програми
+ */
 public class Main {
+    /**
+     * Головний метод програми
+     * @param args аргументи командного рядка
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         List<Integer> numbers = new ArrayList<>();
         boolean running = true;
 
+        // Головний цикл програми
         while (running) {
             System.out.println("Меню: 1 - Згенерувати числа, 2 - Показати результати, 3 - Зберегти, 4 - Оновити, 5 - Вийти");
             int action = scanner.nextInt();
             switch (action) {
                 case 1 -> {
+                    // Генерація випадкових чисел
                     numbers.clear();
                     for (int i = 0; i < 9; i++) {
                         numbers.add(random.nextInt(100));
@@ -189,6 +288,7 @@ public class Main {
                     System.out.println(numbers);
                 }
                 case 2 -> {
+                    // Відображення результатів у консоль
                     Calculator calculator = new Calculator(numbers, new ConsoleResultDisplayFactory());
                     calculator.displayResults();
                 }
@@ -203,7 +303,7 @@ public class Main {
                     calculator.updateData("C:\\Users\\Еля\\Desktop\\ООП\\OOP.Bohomaz.txt");
                     System.out.println("Дані успішно оновлено.");
                 }
-                case 5 -> running = false;
+                case 5 -> running = false; // Вихід з програми
                 default -> System.out.println("Невідома команда");
             }
         }
